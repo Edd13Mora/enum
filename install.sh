d1a6a812abb860accc395c6ac8ced1d4ca04570c
Jonathan Wallace <jwallaceni@gmail.com>
2023-12-30 08:06:01 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-apt install shodan-cli
-apt install nrich
 apt install subfinder
 apt install jq
+pip install -U --user shodan
+wget https://gitlab.com/api/v4/projects/33695681/packages/generic/nrich/latest/nrich_latest_x86_64.deb
+sudo dpkg -i nrich_latest_x86_64.deb
 chmod +x crt.sh initial.sh iteratehosts.sh iterateips.sh awssearchbuckets.sh awsiteratebuckets.sh hunter.sh
 
 echo "Configure Shodan CLI and add Grayhatwarfare API key to config.ini"
+echo "Shodan CLI docs: https://help.shodan.io/command-line-interface/0-installation"
