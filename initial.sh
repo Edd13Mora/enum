79f9116ce5d2cc95d30a170aabb79cc0f1163cae
Jonathan Wallace <jwallaceni@gmail.com>
2023-12-30 07:15:11 +0000

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Usage ./initial.sh <domain> <output file>"
-subfinder -d $1 -o $2
+echo "Usage ./initial.sh <input file> <output file>"
+subfinder -dL $1 -o $2
 echo "Subdomain enumeration complete"
