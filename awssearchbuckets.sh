c39432d181234d25d13010714f9a2faf1b487c42
Jonathan Wallace <jwallaceni@gmail.com>
2023-12-30 07:43:00 +0000

    Update awssearchbuckets.sh

--- awssearchbuckets.sh
+++ awssearchbuckets.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
 APIKEY=$(<config.ini)
-curl --request GET --url 'https://buckets.grayhatwarfare.com/api/v2/buckets?keywords='+$1+'&type=aws' --header 'Authorization: Bearer '"$APIKEY"'' | jq
+curl --request GET --url 'https://buckets.grayhatwarfare.com/api/v2/buckets?keywords='"$1"'&type=aws' --header 'Authorization: Bearer '"$APIKEY"'' | jq
