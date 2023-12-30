1704c789fbc0ee6889ff1c26345bb6336c5086a3
Jonathan Wallace <jwallaceni@gmail.com>
2023-12-30 01:32:08 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ apt install shodan-cli
 apt install nrich
 apt install subfinder
 apt install jq
+chmod +x crt.sh initial.sh iteratehosts.sh iterateips.sh awssearchbuckets.sh awsiteratebuckets.sh hunter.sh
+
+echo "Configure shodan-cli, add Grayhatwarfare API key to config.ini"
