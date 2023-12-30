af916d9f28bd97e6ba9e2fbe9bf74d251c0698a0
Jonathan Wallace <jwallaceni@gmail.com>
2023-12-29 19:47:28 -0500

    Delete awssearchbuckets.sh

--- awssearchbuckets.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl --request GET --url 'https://buckets.grayhatwarfare.com/api/v2/buckets?keywords=website&type=aws' --header 'Authorization: Bearer a9d0c67b6f2cc963d446c61d07ab1192' | jq
