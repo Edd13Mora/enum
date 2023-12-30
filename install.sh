1f0350105fe23d11f1c1db046ff03c87b331aedb
Jonathan Wallace <jwallaceni@gmail.com>
2023-12-30 08:06:47 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ apt install jq
 pip install -U --user shodan
 wget https://gitlab.com/api/v4/projects/33695681/packages/generic/nrich/latest/nrich_latest_x86_64.deb
 sudo dpkg -i nrich_latest_x86_64.deb
+
 chmod +x crt.sh initial.sh iteratehosts.sh iterateips.sh awssearchbuckets.sh awsiteratebuckets.sh hunter.sh
 
 echo "Configure Shodan CLI and add Grayhatwarfare API key to config.ini"
